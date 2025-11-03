c3d34555ad424cc4070dc2521aefa30880cfab23
MaximilianMcC <max@maximilian.co.nz>
2025-11-04 10:47:10 +1300

    restarting (its a 2d topdown shooter now)

--- ShuntingGame/main.cpp
+++ ShuntingGame/main.cpp
@@ -1,16 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include "utils.h"
 #include "numericalVectors.h"
-#include "track.h"
-#include "locomotive.h"
-#include "rollingStock.h"
 #include "assetManager.h"
-#include "trackHandler.h"
 
 int main()
 {
 	// SFML setup
-	sf::RenderWindow window(sf::VideoMode({ 640, 480 }), "Corporate Business Music Playlist (1 hour) Light and Upbeat Background Music For Business");
+	sf::RenderWindow window(sf::VideoMode({ 640, 480 }), "basic as");
 	sf::Clock deltaTimeClock = sf::Clock();
 
 	// Load the debug font
@@ -18,14 +14,6 @@ int main()
 
 	// Share the window so we can draw and whatnot
 	Utils::Init(&window);
-
-	Track* root = new Track(200.0f);
-	root->SetPosition(sf::Vector2f(50, 50));
-	TrackHandler::Add(root);
-	TrackHandler::Add(new Track(100.0f));
-	TrackHandler::Add(new Track(100.0f));
-
-	TrackHandler::ThingsOnTheTrack.push_back(new Locomotive(root));
 
 	// Game window
 	while (window.isOpen())
@@ -40,16 +28,10 @@ int main()
 			if (event->is<sf::Event::Closed>()) window.close();
 		}
 
-		TrackHandler::Update();
-
 		// Draw
 		window.clear(sf::Color::Magenta);
-		TrackHandler::DrawAllTrack();
-		TrackHandler::DrawAllVehicles();
 		window.display();
 	}
-
-	TrackHandler::RemoveAll();
 
 	return 0;
 }
--- ShuntingGame/rollingStock.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "railVehicle.h"
-
-class RollingStock : public RailVehicle
-{
-public:
-	RollingStock(Track* track);
-};
